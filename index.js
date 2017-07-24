5e472e31c3637af74b9d19e9bd16e1d5deb18ff5
Thomas Rory Gummerson <thomas@gummerson.no>
2017-07-24 18:17:47 +0200

    AAAAH WORKING

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ bot.on('new_chat_member', deleteAfter(10 * 60 * 1000));
 bot.on('left_chat_member', deleteAfter(10 * 60 * 1000));
 
 bot.use(async (ctx, next) => {
-	DEBUG && print(ctx.update);
+	DEBUG && ctx.message && print(ctx.message);
 	const banned = await bans.isBanned(ctx.from);
 	if (banned) {
 		return bot.telegram.kickChatMember(ctx.chat.id, ctx.from.id)
@@ -46,42 +46,6 @@ bot.use(async (ctx, next) => {
 			.then(next);
 	}
 	return next();
-});
-
-bot.on('message', async ({ message, chat, reply }) => {
-	if (
-		message.forward_from_chat &&
-		message.forward_from_chat.type !== 'private' &&
-		message.forward_from_chat.username !== 'thedevs'
-	) {
-		const userToWarn = message.from;
-		if (await admins.isAdmin(userToWarn)) {
-			return null;
-		}
-		const reason = 'Forward from unknown ' +
-			message.forward_from_chat.type +
-			': ' + link(message.forward_from_chat);
-		const warnCount = await warns.warn(userToWarn, reason);
-		const promises = [
-			bot.telegram.deleteMessage(chat.id, message.message_id)
-		];
-		if (warnCount < 3) {
-			promises.push(reply(
-				`${link(userToWarn)} warned! (${warnCount}/3)\n` +
-				`Reason: ${reason}`,
-				replyOptions));
-		} else {
-			promises.push(bot.telegram.kickChatMember(chat.id, userToWarn.id));
-			promises.push(bans.ban(userToWarn,
-				'Reached max number of warnings'));
-			promises.push(reply(
-				`${link(userToWarn)} <b>banned</b>! (${warnCount}/3)\n` +
-				'Reason: Reached max number of warnings',
-				replyOptions));
-		}
-		return Promise.all(promises).catch(logError(DEBUG));
-	}
-	return null;
 });
 
 bot.command('warn', async ({ message, chat, reply }) => {
@@ -153,8 +117,48 @@ bot.command('warns', async ({ message, reply }) => {
 	if (!message.reply_to_message) {
 		return reply('Reply to a message');
 	}
-	return reply((await warns.getWarns(message.reply_to_message.from))
-		.join('\n'));
+	let i = 0;
+	const theUser = message.reply_to_message.from;
+	return reply('Warns for ' + link(theUser) + '\n' +
+		(await warns.getWarns(theUser))
+			.map(x => ++i + '. ' + x)
+			.join('\n'), replyOptions);
+});
+
+bot.on('message', async ({ message, chat, reply }) => {
+	if (
+		message.forward_from_chat &&
+		message.forward_from_chat.type !== 'private' &&
+		message.forward_from_chat.username !== 'thedevs'
+	) {
+		const userToWarn = message.from;
+		if (await admins.isAdmin(userToWarn)) {
+			return null;
+		}
+		const reason = 'Forward from ' +
+			message.forward_from_chat.type +
+			': ' + link(message.forward_from_chat);
+		const warnCount = await warns.warn(userToWarn, reason);
+		const promises = [
+			bot.telegram.deleteMessage(chat.id, message.message_id)
+		];
+		if (warnCount < 3) {
+			promises.push(reply(
+				`${link(userToWarn)} warned! (${warnCount}/3)\n` +
+				`Reason: ${reason}`,
+				replyOptions));
+		} else {
+			promises.push(bot.telegram.kickChatMember(chat.id, userToWarn.id));
+			promises.push(bans.ban(userToWarn,
+				'Reached max number of warnings'));
+			promises.push(reply(
+				`${link(userToWarn)} <b>banned</b>! (${warnCount}/3)\n` +
+				'Reason: Reached max number of warnings',
+				replyOptions));
+		}
+		return Promise.all(promises).catch(logError(DEBUG));
+	}
+	return null;
 });
 
 bot.startPolling();
