8590b58eca4d07dc433e45a2d6d5cd710e877d9e
GingerPlusPlus <woj.pawlik@gmail.com>
2017-10-04 20:02:09 +0200

    Print a message and leave if couldn't obtain invite link
    Closes #25

--- handlers/middlewares/addedToGroup.js
+++ handlers/middlewares/addedToGroup.js
@@ -1,7 +1,6 @@
 'use strict';
 
 // Bot
-const bot = require('../../bot');
 const { replyOptions } = require('../../bot/options');
 
 const { admin } = require('../../stores/user');
@@ -10,14 +9,23 @@ const { masterID } = require('../../config.json');
 
 const addedToGroupHandler = async (ctx, next) => {
 	const msg = ctx.message;
+	const { telegram } = ctx;
 
 	const wasAdded = msg.new_chat_members.some(user =>
 		user.username === ctx.me);
 	if (wasAdded && ctx.from.id === masterID) {
 		await admin(ctx.from);
 		if (!await managesGroup(ctx.chat)) {
-			const link = await bot.telegram.exportChatInviteLink(ctx.chat.id);
-			ctx.chat.link = link ? link : '';
+			try {
+				const link = await telegram.exportChatInviteLink(ctx.chat.id);
+				ctx.chat.link = link ? link : '';
+			} catch (err) {
+				await ctx.replyWithHTML(
+					'⚠️ <b>Please re-add me with admin permissions.</b>' +
+					'\n\n' +
+					`<code>${err}</code>`);
+				return telegram.leaveChat(ctx.chat.id);
+			}
 			await addGroup(ctx.chat);
 		}
 		ctx.reply('🛠 <b>Ok, I\'ll help you manage this group from now.</b>',
