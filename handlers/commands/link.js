57ff83b779d1a391fde5154643a172d6cb50b589
GingerPlusPlus <woj.pawlik@gmail.com>
2018-02-17 22:19:00 +0100

    /link doesn't try to reply to PMs

--- handlers/commands/link.js
+++ handlers/commands/link.js
@@ -6,7 +6,11 @@ const { scheduleDeletion } = require('../../utils/tg');
 // DB
 const { managesGroup } = require('../../stores/group');
 
-const linkHandler = async ({ chat, replyWithHTML }) => {
+const linkHandler = async ({ chat, replyWithHTML }, next) => {
+	if (chat.type === 'private') {
+		return next();
+	}
+
 	const group = await managesGroup({ id: chat.id });
 
 	return replyWithHTML('ℹ️ <b>Group\'s link:</b>\n\n' +
