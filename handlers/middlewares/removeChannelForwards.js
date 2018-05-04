3e6a242a6b4e453cbf7307c36c5f0062ea122d71
GingerPlusPlus <woj.pawlik@gmail.com>
2018-05-04 15:48:07 +0200

    New handler: middlewares/removeChannelForwards (#51)

---
+++ handlers/middlewares/removeChannelForwards.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const R = require('ramda');
+const { optional, passThru } = require('telegraf');
+
+const { excludeLinks = [] } = require('../../config');
+
+if (excludeLinks === false || excludeLinks === '*') {
+	module.exports = passThru();
+	return;
+}
+
+const isChannelForward = R.pathEq(
+	[ 'message', 'forward_from_chat', 'type' ],
+	'channel'
+);
+const fromAdmin = R.pathEq([ 'from', 'status' ], 'admin');
+
+const capturingGroups = R.tail;
+
+const toUsername = R.compose(
+	capturingGroups,
+	R.match(/^(?:@|(?:https?:\/\/)?(?:t\.me|telegram\.(?:me|dog))\/)(\w+)/i)
+);
+
+const customWhitelist = R.pipe(
+	R.chain(toUsername),
+	R.map(R.toLower),
+	R.constructN(1, Set),
+)(excludeLinks);
+
+const isWhitelisted = username => customWhitelist.has(username.toLowerCase());
+
+const fromWhitelisted = ctx =>
+	isWhitelisted(ctx.message.forward_from_chat.username || '');
+
+const pred = R.allPass([
+	isChannelForward,
+	R.complement(fromAdmin),
+	R.complement(fromWhitelisted),
+]);
+
+const handler = ctx => {
+	ctx.deleteMessage();
+	return ctx.warn({
+		admin: ctx.botInfo,
+		reason: 'Channel forward',
+		userToWarn: ctx.from,
+	});
+};
+
+module.exports = optional(pred, handler);
