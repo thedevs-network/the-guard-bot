498c76ae0db9be3d2a70417d62c56558bcf7a64b
GingerPlusPlus <woj.pawlik@gmail.com>
2018-05-05 20:10:22 +0200

    [Bugfix] handle cyclic dependency

--- bot/index.js
+++ bot/index.js
@@ -9,6 +9,8 @@ if (process.env.NODE_ENV === 'development') {
 	bot.polling.offset = -1;
 }
 
-Object.assign(bot.context, require('./context'));
-
 module.exports = bot;
+
+// cyclic dependency
+// bot/index requires context requires actions/warn requires bot/index
+Object.assign(bot.context, require('./context'));
