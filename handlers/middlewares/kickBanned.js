b11a35a503ee6900e9fc174738d6e80a5918adf6
SitiSchu <admin@sitischu.com>
2018-09-09 19:48:34 +0200

    Remove message for already banned members
    Signed-off-by: SitiSchu <admin@sitischu.com>

--- handlers/middlewares/kickBanned.js
+++ handlers/middlewares/kickBanned.js
@@ -1,26 +1,12 @@
 'use strict';
 
-const dedent = require('dedent-js');
-
-// Utils
-const { link } = require('../../utils/tg');
-
-const { replyOptions } = require('../../bot/options');
-
-const kickBannedHandler = async (ctx, next) => {
+const kickBannedHandler = (ctx, next) => {
 	if (ctx.chat.type === 'private') {
 		return next();
 	}
 	if (ctx.from.status === 'banned') {
 		ctx.deleteMessage();
-		await ctx.kickChatMember(ctx.from.id);
-		return ctx.replyWithHTML(
-			dedent(`
-			🚫 ${link(ctx.from)} <b>is banned</b>!
-
-			Reason: ${ctx.from.ban_reason}`),
-			replyOptions
-		);
+		return ctx.kickChatMember(ctx.from.id);
 	}
 	return next();
 };
