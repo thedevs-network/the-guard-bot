1fbec32df3368c2d1c83853725d7b2826d83247d
GingerPlusPlus <woj.pawlik@gmail.com>
2019-01-31 21:54:43 +0100

    Extract target user from bot's messages

--- utils/parse.js
+++ utils/parse.js
@@ -11,6 +11,12 @@ const isTextMention = m => m.type === 'text_mention';
 const spliceOut = (s, { offset, length }) =>
 	s.slice(0, offset) + s.slice(offset + length);
 
+const botReply = ({ from, entities = [] }) => {
+	const textMentions = entities.filter(isTextMention);
+
+	return from.is_bot && textMentions.length === 2 && [ textMentions[1].user ];
+};
+
 const parse = message => {
 	// eslint-disable-next-line no-empty-character-class
 	const regex = /^\/\w+(?:@\w+)?((?:@\w+|\d+|\s+)*)(.*)$/s;
@@ -20,9 +26,12 @@ const parse = message => {
 
 	const [ , ids, reason ] = regex.exec(noTextMentions);
 	const users = textMentions.concat(ids.match(/@\w+|\d+/g) || []);
+	const { reply_to_message } = message;
 	const targets = users.length
 		? users.map(toUserObject)
-		: message.reply_to_message ? [ message.reply_to_message.from ] : [];
+		: reply_to_message
+			? botReply(reply_to_message) || [ reply_to_message.from ]
+			: [];
 
 	return { reason, targets };
 };
