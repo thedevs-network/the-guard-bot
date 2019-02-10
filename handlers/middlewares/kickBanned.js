bdc10e1702b3ea00c13dd9c689762a14cd431422
GingerPlusPlus <woj.pawlik@gmail.com>
2019-02-10 17:04:16 +0100

    Add error message to kickBanned

--- handlers/middlewares/kickBanned.js
+++ handlers/middlewares/kickBanned.js
@@ -1,12 +1,15 @@
 'use strict';
 
+const noop = Function.prototype;
+
 const kickBannedHandler = (ctx, next) => {
 	if (!ctx.chat.type.endsWith('group')) {
 		return next();
 	}
 	if (ctx.from.status === 'banned') {
-		ctx.deleteMessage();
-		return ctx.kickChatMember(ctx.from.id);
+		ctx.deleteMessage().catch(noop);
+		return ctx.kickChatMember(ctx.from.id)
+			.catch(err => ctx.reply(`⚠️ kickBanned: ${err}`));
 	}
 	return next();
 };
