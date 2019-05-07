b4e002e091439789740ba0084ec571401b73ea5a
YouTwitFace <contact@lungers.com>
2019-05-07 07:45:12 -0400

    Disable web page preview on the anti farsi reply and fix linting errors

--- plugins/anti-farsi.js
+++ plugins/anti-farsi.js
@@ -1,22 +1,36 @@
+'use strict';
+
 const FARSI_RE = /[\u0600-\u06FF]/;
 
 module.exports = ({ message, chat, replyWithMarkdown }, next) => {
-    if (!message) {
-        return next();
-    }
+	if (!message) {
+		return next();
+	}
 
-    const { text } = message;
-    const replyOptions = { reply_to_message_id: message.message_id };
+	const { text } = message;
+	const replyOptions = {
+		reply_to_message_id: message.message_id,
+		disable_web_page_preview: true,
+	};
 
-    if (!text || !['group', 'supergroup'].includes(chat.type) || chat.id === -1001211669317) {
-        return next();
-    }
+	if (
+		!text ||
+        ![ 'group', 'supergroup' ].includes(chat.type) ||
+        chat.id === -1001211669317
+	) {
+		return next();
+	}
 
-    const isFarsi = FARSI_RE.test(text);
+	const isFarsi = FARSI_RE.test(text);
 
-    if (isFarsi) {
-        replyWithMarkdown('Please only send proxies here and write in English. For farsi chatting join [the farsi group](https://t.me/joinchat/E7Xrc0g4m0VlTV7oHyu8EQ).', replyOptions);
-    }
+	if (isFarsi) {
+		replyWithMarkdown(
+			'Please only send proxies here and write in English. ' +
+            'For farsi chatting join [the farsi group]' +
+            '(https://t.me/joinchat/E7Xrc0g4m0VlTV7oHyu8EQ).',
+			replyOptions
+		);
+	}
 
-    return next();
-}
+	return next();
+};
