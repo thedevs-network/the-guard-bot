5649aae8b9234520b60cf0800367dc088780546c
Wojciech Pawlik <woj.pawlik@gmail.com>
2020-02-17 17:05:06 +0100

    Silence "message already deleted" errors

---
+++ handlers/middlewares/monkeyPatch.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const msgAlreadyDeleted = 'Bad Request: message to delete not found';
+
+module.exports = (ctx, next) => {
+	ctx.tg.deleteMessage = async (chat_id, message_id) => {
+		try {
+			return await ctx.tg.callApi('deleteMessage', {
+				chat_id,
+				message_id
+			});
+		} catch (err) {
+			if (err.description === msgAlreadyDeleted) {
+				return false;
+			}
+			throw err;
+		}
+	};
+	return next();
+};
