b710a4720963bf2e9d40955877f74592376e80f1
Martin Rys <martin@rys.pw>
2020-10-09 10:42:43 +0200

    handle all variations of /!@report|admins
    Co-authored-by: Thomas Rory Gummerson <thomas@gummerson.no>

--- handlers/regex/index.js
+++ handlers/regex/index.js
@@ -5,7 +5,7 @@ const { compose, hears } = require('telegraf');
 /* eslint-disable global-require */
 
 module.exports = compose([
-	hears(/^(?:!report|[@!]admins?)\b/i, require('../commands/report')),
+	hears(/^(?:[/@!](report|admins?))\b/i, require('../commands/report')),
 	require('./runCustomCmd'),
 	require('./groupLinker'),
 ]);
