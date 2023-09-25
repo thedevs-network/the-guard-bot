970a12ed273e547f62c5e6ec3ac65c5ebdc5d4fa
Martin Rys <martin@rys.pw>
2023-09-25 12:26:38 +0200

    Make warning expiration wording more obvious

--- actions/warn.js
+++ actions/warn.js
@@ -41,7 +41,7 @@ module.exports = async ({ admin, amend, reason, userToWarn }) => {
 	const warnMessage = html`
 		⚠️ ${lrm}${admin.first_name} <b>warned</b> ${link(userToWarn)}.
 		${count}: ${lrm}${reason}
-		<i>${typeof expireWarnsAfter === 'undefined' || expireWarnsAfter === Infinity ? '' : `Expires on ${yyyymmdd(new Date(date.getTime() + ms(expireWarnsAfter)))}`}</i>
+		<i>${typeof expireWarnsAfter === 'undefined' || expireWarnsAfter === Infinity ? '' : `This warning expires on ${yyyymmdd(new Date(date.getTime() + ms(expireWarnsAfter)))}`}</i>
 	`;
 
 	if (recentWarns.length >= numberOfWarnsToBan) {
